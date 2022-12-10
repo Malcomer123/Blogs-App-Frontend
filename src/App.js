6e1a981fdfc781e17bb3a282e50800f72251a73c
Ben-hammad Ali <alibenhammad7@gmail.com>
2022-12-10 12:04:45 +0100

    login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {Index} from "./Components";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Index } from "./Components";
+import SignIn from "./Components/SignIn";
 import Layout from "./Components/Layout/Layout";
 
 function App() {
-    return (
+  return (
     <div className="App">
-    <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-            <Route path="/index" element={<Layout/>}>
-                <Route index element={<Index/>}/>
-                <Route path="blog" element={(<div>hi</div>)}/>
-                <Route path="roar" element={(<div>oktazt</div>)}/>
-            </Route>
+          <Route path="/index" element={<Layout />}>
+            <Route index element={<Index />} />
+            <Route path="blog" element={<div>hi</div>} />
+            <Route path="roar" element={<div>oktazt</div>} />
+          </Route>
+          <Route path="/login" element={<SignIn />} />
         </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,6 @@
 /* Pages & Layout */
-export {default as Index} from './Pages/Index/index'
-export {default as Layout} from './Layout/Layout'
+export { default as Index } from "./Pages/Index/index";
+export { default as Layout } from "./Layout/Layout";
+
+
+
